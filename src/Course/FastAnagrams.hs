f15c6ec4c6d3580cd178035ad11f2c06463a881c
Zhansong Li <lizhansong@hvariant.com>
2019-10-27 00:22:51 +1100

    'fast' anagram

--- src/Course/FastAnagrams.hs
+++ src/Course/FastAnagrams.hs
@@ -6,18 +6,23 @@ module Course.FastAnagrams where
 import Course.Core
 import Course.List
 import Course.Functor
+import Course.Applicative
 import qualified Data.Set as S
 
 -- Return all anagrams of the given string
 -- that appear in the given dictionary file.
 -- on a Mac - run this with:
 -- > fastAnagrams "Tony" "/usr/share/dict/words"
+
+
 fastAnagrams ::
   Chars
   -> FilePath
   -> IO (List Chars)
-fastAnagrams =
-  error "todo: Course.FastAnagrams#fastAnagrams"
+fastAnagrams s path = map ncString . filter (flip S.member $ perms) <$> words'
+  where words' = map NoCaseString <$> lines <$> readFile path
+        perms :: S.Set NoCaseString
+        perms = S.fromList . hlist . map NoCaseString $ permutations s
 
 newtype NoCaseString =
   NoCaseString {
@@ -26,6 +31,9 @@ newtype NoCaseString =
 
 instance Eq NoCaseString where
   (==) = (==) `on` map toLower . ncString
+
+instance Ord NoCaseString where
+  compare = compare `on` map toLower . ncString
 
 instance Show NoCaseString where
   show = show . ncString
